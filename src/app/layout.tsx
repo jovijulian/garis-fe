5b536c8b99fc932edcfbc6fdce0ddd03247718b8
jovijulian <jovijulian2002@gmail.com>
2026-01-07 11:57:37 +0700

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 "use client";
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
 import { Outfit } from 'next/font/google';
 import './globals.css';
 
