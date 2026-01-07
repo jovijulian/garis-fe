7644f4fa5c8b9512172e5fd45bff3bed653c61f0
jovijulian <jovijulian2002@gmail.com>
2026-01-07 11:16:31 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  generateEtags: false,
   /* config options here */
   webpack(config) {
     config.module.rules.push({
